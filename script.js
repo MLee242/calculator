b29648e9a65dd268eaaf0db462474619a31b7229
buddybuddymoon <buddybuddymoon@gmail.com>
2019-11-27 10:09:38 -0500

    add keyboard function

--- script.js
+++ script.js
@@ -59,22 +59,24 @@ function operate(x,y,op){
 }
 
 
-function checkstart(){
-
-    if(this.id == 21){
+function checkstart(button){
+    
+    console.log(button.id);
+    if(button.id == 21){
         if(!powerOn){
             powerOn = true;
             powerlight.style.backgroundColor="red";
-            this.style.border = "2px solid rgb(249,166,179)";
+            button.style.border = "2px solid rgb(249,166,179)";
             screen.textContent = `${displaynumber}`;
             const audio = document.querySelector(`audio[data-key="1"]`);
             if (!audio) return;;
             audio.currentTime = 0;
             audio.play();
+            
         }else{
             powerOn = false;
             powerlight.style.backgroundColor='#b98181';
-            this.style.border = "0px";
+            button.style.border = "0px";
             screen.textContent = "";
             formuladisplay.textContent =" ";
             prev = curr = undefined;
@@ -82,9 +84,9 @@ function checkstart(){
         }
     }
     if(powerOn && !disable){
-        if(this.id == 1){
+        if(button.id == 1){
             settodefault();
-        }else if(this.id == 2){
+        }else if(button.id == 2){
             if(curr != undefined){
                 curr *= -1;
                 displaynumber = curr;
@@ -93,37 +95,37 @@ function checkstart(){
                 displaynumber = prev;
             }
             
-        }else if(this.id == 3){
+        }else if(button.id == 3){
             setoperator('%');
-        }else if(this.id == 4){
+        }else if(button.id == 4){
             setoperator('/');
-        }else if(this.id == 5){
+        }else if(button.id == 5){
             updatenumber(7);
-        }else if(this.id == 6){
+        }else if(button.id == 6){
             updatenumber(8);
-        }else if(this.id == 7){
+        }else if(button.id == 7){
             updatenumber(9);
-        }else if(this.id == 8){
+        }else if(button.id == 8){
             setoperator('*');
-        }else if(this.id == 9){
+        }else if(button.id == 9){
             updatenumber(4);
-        }else if(this.id == 10){
+        }else if(button.id == 10){
             updatenumber(5);
-        }else if(this.id == 11){
+        }else if(button.id == 11){
             updatenumber(6);
-        }else if(this.id == 12){
+        }else if(button.id == 12){
             setoperator('-');
-        }else if(this.id == 13){
+        }else if(button.id == 13){
             updatenumber(1);
-        }else if(this.id == 14){
+        }else if(button.id == 14){
             updatenumber(2);
-        }else if(this.id == 15){
+        }else if(button.id == 15){
             updatenumber(3);
-        }else if(this.id == 16){
+        }else if(button.id == 16){
             setoperator('+');
-        }else if(this.id == 17){
+        }else if(button.id == 17){
             updatenumber(0);
-        }else if(this.id == 18){
+        }else if(button.id == 18){
             
             if(!decimaldigit){
                 if(curr == undefined){
@@ -135,16 +137,16 @@ function checkstart(){
             }
             
 
-        }else if(this.id == 19){
+        }else if(button.id == 19){
             formuladisplay.textContent = " ";
             evaluate();
-        }else if(this.id == 20){
+        }else if(button.id == 20){
             backspace();
         }
 
         updateDisplay();
     }else if(powerOn && disable){
-        if(this.id == 1){
+        if(button.id == 1){
             settodefault();
             disable = false;
             updateDisplay();
@@ -153,7 +155,7 @@ function checkstart(){
     }
 
 
-
+    button.blur();
 }
 
 function settodefault(){
@@ -264,11 +266,9 @@ function backspace(){
    
     if(decimaldigit){
         curr = curr.substring(0,curr.length-1);
-        console.log(curr);
         
         if(curr.indexOf('.') == -1){
             decimaldigit = false;
-            console.log("hi");
         }
 
     }else{
@@ -294,18 +294,97 @@ function changebg(){
 
     this.style.backgroundColor = "gold";
 }
-function backbg(){
-    if(this.id < 20){
-        this.style.backgroundColor = "transparent";
+function backbg(button){
+    if(button.id < 20){
+        button.style.backgroundColor = "transparent";
     }else{
-        this.style.backgroundColor = "#999";
+        button.style.backgroundColor = "#999";
     }
     
 }
 
 
+
 buttons.forEach(button =>{ 
-    button.addEventListener('click', checkstart);
+    button.addEventListener('click', function(){
+        
+        checkstart(button);
+    });
     button.addEventListener('mouseover', changebg);
-    button.addEventListener('mouseout', backbg);
+    button.addEventListener('mouseout', function(){
+        
+        backbg(button);
+    });
 });
+
+
+
+document.onkeydown = function(event){
+
+
+    let k = checkingkey(event, event.shiftKey);
+    console.log(k);
+  
+    if(k != null){
+        checkstart(k);
+        k.style.backgroundColor = "gold";
+    }
+}
+
+
+function checkingkey(event, shiftstatus){
+    let k = null;
+    console.log(shiftstatus);
+    if(event.keyCode == 49 || event.keyCode == 97){
+        k = buttons[12];
+    }else if(event.keyCode == 50 || event.keyCode == 98){
+        k = buttons[13];
+    }else if(event.keyCode == 51 || event.keyCode == 99){
+        k = buttons[14];
+    }else if(event.keyCode == 52 || event.keyCode == 100){
+        k = buttons[8];
+    }else if(!shiftstatus && event.keyCode == 53 || event.keyCode == 101){
+        k = buttons[9];
+    }else if(event.keyCode == 54 || event.keyCode == 102){
+        k = buttons[10];
+    }else if(event.keyCode == 55 || event.keyCode == 103){
+        k = buttons[4];
+    }else if(!shiftstatus && event.keyCode == 56 || event.keyCode == 104){
+        k = buttons[5];
+    }else if(event.keyCode == 57 || event.keyCode == 105){
+        k = buttons[6];
+    }else if(event.keyCode == 106 || shiftstatus && event.keyCode == 56){
+        k = buttons[7];
+    }else if(event.keyCode == 107 || shiftstatus && event.keyCode == 61){
+        k = buttons[15];
+    }else if(event.keyCode == 173 || event.keyCode == 109){
+        k = buttons[11];
+    }else if(event.keyCode == 111 || event.keyCode == 191){
+        k = buttons[3];
+    }else if(event.keyCode == 61 || event.keyCode == 13){
+        k = buttons[18];
+    }else if(event.keyCode == 190 || event.keyCode == 110){
+        k = buttons[17];
+    }else if(event.keyCode == 48 || event.keyCode == 96){
+        k = buttons[16];
+    }else if(shiftstatus && event.keyCode == 53 ){
+        k = buttons[2];
+    }else if(event.keyCode == 8){
+        k = buttons[19];
+    }
+    return k;
+    
+}
+
+document.onkeyup = function(event){
+    let k = checkingkey(event, event.shiftKey);
+    
+  
+    if(k != null){
+        backbg(buttons[7]);
+        backbg(buttons[15]);
+        backbg(buttons[2]);
+        backbg(k);
+    }
+
+}
